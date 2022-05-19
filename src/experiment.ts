634544135e768390ace8644ebd668371830b5c50
Robin Bürkli <robuba.jr@gmx.ch>
2022-05-19 17:27:10 +0200

    Add pre and post survey

--- src/experiment.ts
+++ src/experiment.ts
@@ -65,6 +65,7 @@ import { getStaircaseDetectionTimeline } from './staircaseDetectionTimeline';
 import { ParticipantVisibilityThreshold } from './ParticipantVisibilityThreshold';
 import { getPracticeImaginationTimeline } from './practiceImaginationTimeline';
 import { getMainExperimentTimeline } from './mainExperimentTimeline';
+import SurveyHtmlFormPlugin from '@jspsych/plugin-survey-html-form';
 
 /**
  * This method will be executed by jsPsych Builder and is expected to run the
@@ -83,6 +84,12 @@ import { getMainExperimentTimeline } from './mainExperimentTimeline';
 export async function run({ assetPaths, input = {}, environment }) {
   // Initiate the jsPsych object
   const jsPsych = initJsPsych();
+
+  // Generate and save participant ID
+  const participantId = new Date().valueOf();
+  jsPsych.data.addProperties({
+    participant_id: participantId,
+  });
 
   // Define the main timeline array
   const timeline = [];
@@ -112,17 +119,110 @@ export async function run({ assetPaths, input = {}, environment }) {
     images: [...backgroundNoiseFrames, [...assetPaths.images]],
   });
 
-  // Push the welcome screen to the timeline
+  // First trial presenting participant id and informing about rights
   timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    stimulus: `
-      <h1>
-        Willkommen zum Experiment!
-      </h1>
-      <p>
-        Drücken Sie eine beliebige Taste, um zu starten.
-      </p>
-    `,
+    type: SurveyHtmlFormPlugin,
+    html: `<h3>Willkommen</h3>
+  <p>
+    Vielen Dank, dass Sie an der Studie teilnehmen. Zunächst werden Sie ein paar
+    Informationen zum Datenschutz und zu Ihren Rechten erhalten.
+  </p>
+  <p>
+    Die Daten, welche wir durch Ihre Teilnahme erheben, werden innerhalb der
+    Schweiz auf einem Server der Universität Bern gespeichert. Sie sind
+    anonymisiert, sodass keine Rückschlüsse auf Ihre Person möglich sind.
+  </p>
+  <p>
+    Sie dürfen die Studie jederzeit abbrechen, ohne, dass Ihnen daraus Nachteile
+    entstehen. Ihre Daten werden nicht verwendet, wenn Sie die Studie nicht
+    abschliessen.
+  </p>
+  <p>
+    Falls Sie zu einem späteren Zeitpunkt möchten, dass Ihre Daten doch nicht
+    verwendet werden, können Sie ohne Konsequenzen eine Löschung beantragen.
+    Kontaktieren Sie dafür bitte die Studienleitung.
+  </p>
+  <p>
+    Damit wir Ihre (anonymisierten) Daten in diesem Fall identifizieren können,
+    werden sie mit einer einzigartigen ID versehen. Diese ID wird Ihnen jetzt
+    <strong>genau einmal</strong> angezeigt. Bitte notieren Sie diese für den
+    Fall, dass Sie ihre Daten später löschen lassen möchten.
+  </p>
+  <p>ID: <strong>${participantId}</strong></p>
+  <p>
+    <label>
+      <input
+        name="checkbox_agree"
+        id="checkbox_agree"
+        type="checkbox"
+        class="checkbox-input"
+        required
+      />
+      Hiermit bestätige ich, dass ich freiwillig an dieser Studie teilnehme, die
+      Informationen gelesen und verstanden habe und den Bedingungen zustimme.
+      Meine Daten-ID habe ich notiert.
+    </label>
+  </p>
+  <div class="vertical_spacer"></div>
+  `,
+    button_label: 'Weiter',
+  });
+
+  // First trial capturing demographics
+  timeline.push({
+    type: SurveyHtmlFormPlugin,
+    html: `<p>
+  Bevor wir mit dem eigentlichen Experiment starten, haben wir noch ein paar
+  Fragen zu ihrer Person.
+</p>
+<p>
+  <fieldset>
+    <legend>Welchem Geschlecht fühlen Sie sich zugehörig?</legend>
+    <div>
+      <input type="radio" id="option_gender_diverse" name="gender" value="diverse" required>
+      <label for="option_gender_diverse">Divers</label>
+    </div>
+    <div>
+      <input type="radio" id="option_gender_female" name="gender" value="female" required>
+      <label for="option_gender_female">Weiblich</label>
+    </div>
+    <div>
+      <input type="radio" id="option_gender_male" name="gender" value="male" required>
+      <label for="option_gender_male">Männlich</label>
+    </div>
+  </fieldset>
+  <div class="vertical_spacer"></div>
+  <fieldset>
+    <legend>Wie alt sind Sie in Jahren?</legend>
+    <div>
+      <input type="number" id="age" name="age" required>
+    </div>
+  </fieldset>
+  <div class="vertical_spacer"></div>
+  <fieldset>
+    <legend>Wie ist Ihre Händigkeit?</legend>
+    <div>
+      <input type="radio" id="option_handedness_left" name="handedness" value="left" required>
+      <label for="option_handedness_left">Linkshändig</label>
+    </div>
+    <div>
+      <input type="radio" id="option_handedness_right" name="handedness" value="right" required>
+      <label for="option_handedness_right">Rechtshändig</label>
+    </div>
+    <div>
+      <input type="radio" id="option_handedness_both" name="handedness" value="both" required>
+      <label for="option_handedness_both">Beidhändig</label>
+    </div>
+  </fieldset>
+  <div class="vertical_spacer"></div>
+</p>`,
+    button_label: 'Weiter',
+    data: {
+      test_part: 'survey_demographics',
+    },
+    on_finish: (data: any) => {
+      jsPsych.data.addProperties(data.response);
+    },
   });
 
   // Push the main explanation of the experiment to the timeline
@@ -155,34 +255,34 @@ export async function run({ assetPaths, input = {}, environment }) {
   });
 
   // Add practice trials
-  // timeline.push(
-  //   getPraciceDetectionTimeline(
-  //     jsPsych,
-  //     responseMapping,
-  //     backgroundNoiseFrames,
-  //     fixationCrossTrial
-  //   )
-  // );
+  timeline.push(
+    getPraciceDetectionTimeline(
+      jsPsych,
+      responseMapping,
+      backgroundNoiseFrames,
+      fixationCrossTrial
+    )
+  );
 
   // Add staircase sub-timeline
-  // timeline.push(
-  //   getStaircaseDetectionTimeline(
-  //     jsPsych,
-  //     responseMapping,
-  //     backgroundNoiseFrames,
-  //     fixationCrossTrial,
-  //     participantVisibilityThreshold
-  //   )
-  // );
+  timeline.push(
+    getStaircaseDetectionTimeline(
+      jsPsych,
+      responseMapping,
+      backgroundNoiseFrames,
+      fixationCrossTrial,
+      participantVisibilityThreshold
+    )
+  );
 
   // Add imagination practice sub-timeline
-  // timeline.push(
-  //   getPracticeImaginationTimeline(
-  //     jsPsych,
-  //     fixationCrossTrial,
-  //     backgroundNoiseFrames
-  //   )
-  // );
+  timeline.push(
+    getPracticeImaginationTimeline(
+      jsPsych,
+      fixationCrossTrial,
+      backgroundNoiseFrames
+    )
+  );
 
   // Add main experiment sub-timeline
   timeline.push(
@@ -194,6 +294,50 @@ export async function run({ assetPaths, input = {}, environment }) {
       participantVisibilityThreshold
     )
   );
+
+  // Last trial asking final questions
+  timeline.push({
+    type: SurveyHtmlFormPlugin,
+    html: `<p>
+  Geschafft. Wir haben noch ein paar abschliessende Fragen an Sie, dann sind wir fertig.
+</p>
+<p>
+  <fieldset>
+    <legend>Haben Sie sich die Gittermuster in den Blöcken wirklich vorgestellt,
+      als wir Sie darum baten? (Die Antwort auf diese Frage wird Ihre
+      Belonung nicht beeinflussen.)</legend>
+    <div>
+      <input type="radio" id="option_imagination_yes" name="did_participant_really_imagine" value="yes" required>
+      <label for="option_imagination_yes">Ja</label>
+    </div>
+    <div>
+      <input type="radio" id="option_imagination_no" name="did_participant_really_imagine" value="no" required>
+      <label for="option_imagination_no">Nein</label>
+    </div>
+  </fieldset>
+  <div class="vertical_spacer"></div>
+  <fieldset>
+    <legend>Haben Sie das Gefühl, dass sich das Vorstellen der Gittermuster auf
+      Ihre Antworten bei der Aufgabe ausgewirkt hat?</legend>
+    <div>
+      <input type="radio" id="option_influence_feeling_yes" name="did_participant_feel_influence_of_imagination" value="yes" required>
+      <label for="option_influence_feeling_yes">Ja</label>
+    </div>
+    <div>
+      <input type="radio" id="option_influence_feeling_no" name="did_participant_feel_influence_of_imagination" value="no" required>
+      <label for="option_influence_feeling_no">Nein</label>
+    </div>
+  </fieldset>
+</p>
+<p>Klicken Sie auf weiter, um Ihre Daten einzureichen.</p> `,
+    button_label: 'Weiter',
+    data: {
+      test_part: 'survey_demographics',
+    },
+    on_finish: (data: any) => {
+      jsPsych.data.addProperties(data.response);
+    },
+  });
 
   // Run the experiment
   await jsPsych.run(timeline);
